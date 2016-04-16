c29fc563eb997dd63f2c8e4c13ccf233a4e79292
yibo <yiboyang@ymail.com>
2016-04-16 17:24:58 -0500

    add example marker and infowindow

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 
 
-var map;
 function initMap() {
 //  var directionsService = new google.maps.DirectionsService;
 //  var directionsDisplay = new google.maps.DirectionsRenderer;
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 7,
-    center: {lat: 33.0149231, lng: -96.9464376}
-  });
+    var here = {lat: 33.0149231, lng: -96.9464376};
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 7,
+        center: {lat: 33.0149231, lng: -96.9464376}
+      });
+
+    var contentString = '<div id="content">'+
+          '</div>'+
+          '<h1 id="firstHeading" class="firstHeading">HERE</h1>'+
+          '<div id="bodyContent">'+
+          '<p>Gilley\'s Dallas</p>'+
+          '</div>';
+
+    var infowindow = new google.maps.InfoWindow({
+      content: contentString
+    });
+
+    var marker = new google.maps.Marker({
+      position: here,
+      map: map,
+      title: 'Here (Gilley\'s Dallas)'
+    });
+    marker.addListener('click', function() {
+      infowindow.open(map, marker);
+    });
 }
